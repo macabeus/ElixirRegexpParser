6ee7b267d66e4d8b7d662bfc60c0381775eaa37b
Bruno Macabeus Aquino <bruno.macabeus@gmail.com>
2017-10-29 00:14:43 -0300

    refactor(quantifier): minor readable improvement in anonymous functions

--- lib/Parses/Quantifier.ex
+++ lib/Parses/Quantifier.ex
@@ -22,19 +22,19 @@ defmodule RegexpParser.Quantifier do
   def parser_braces_full() do # {n,m}
     pipe([
       char("{"), integer(), char(","), integer(), char("}")
-    ], fn p -> [_, min, _, max, _] = p; %Quantifier{min: min, max: max} end)
+    ], fn [_, min, _, max, _] -> %Quantifier{min: min, max: max} end)
   end
 
   def parser_braces_at_least() do # {n,}
     pipe([
       char("{"), integer(), char(","), char("}")
-    ], fn p -> [_, min, _, _] = p; %Quantifier{min: min} end)
+    ], fn [_, min, _, _] -> %Quantifier{min: min} end)
   end
 
   def parser_braces_exactly() do # {n}
     pipe([
       char("{"), integer(), char("}")
-    ], fn p -> [_, value, _] = p; %Quantifier{min: value, max: value} end)
+    ], fn [_, value, _] -> %Quantifier{min: value, max: value} end)
   end
 
   # Parse for +, * and ?
