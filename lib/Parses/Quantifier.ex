a22c42b108e151fb09a2bde586a41b994b2cd2a5
Bruno Macabeus Aquino <bruno.macabeus@gmail.com>
2017-10-28 23:59:19 -0300

    refactor(quantifier): more readable

--- lib/Parses/Quantifier.ex
+++ lib/Parses/Quantifier.ex
@@ -18,53 +18,23 @@ defmodule RegexpParser.Quantifier do
     ])
   end
 
+  # Parser for explit quantifier
   def parser_braces_full() do # {n,m}
     pipe([
-      quantifier_brace_open(), integer(), comma(), integer(), quantifier_brace_close()
+      char("{"), integer(), char(","), integer(), char("}")
     ], fn p -> [_, min, _, max, _] = p; %Quantifier{min: min, max: max} end)
   end
 
   def parser_braces_at_least() do # {n,}
     pipe([
-      quantifier_brace_open(), integer(), comma(), quantifier_brace_close()
+      char("{"), integer(), char(","), char("}")
     ], fn p -> [_, min, _, _] = p; %Quantifier{min: min} end)
   end
 
   def parser_braces_exactly() do # {n}
     pipe([
-      quantifier_brace_open(), integer(), quantifier_brace_close()
+      char("{"), integer(), char("}")
     ], fn p -> [_, value, _] = p; %Quantifier{min: value, max: value} end)
-  end
-
-  # Parser for {n,m}
-  defparser quantifier_brace_open(%ParserState{status: :ok, column: col, input: <<?{::utf8,rest::binary>>, results: results} = state) do
-    %{state | :column => col + 1, :input => rest, :results => ["{"|results]}
-  end
-  defp quantifier_brace_open_impl(%ParserState{status: :ok, line: line, column: col, input: <<c::utf8,_::binary>>} = state) do
-    %{state | :status => :error, :error => "Expected { found `#{<<c::utf8>>}` at line #{line}, column #{col + 1}."}
-  end
-  defp quantifier_brace_open_impl(%ParserState{status: :ok, input: <<>>} = state) do
-    %{state | :status => :error, :error => "Expected {, but hit end of input."}
-  end
-
-  defparser comma(%ParserState{status: :ok, column: col, input: <<?,::utf8,rest::binary>>, results: results} = state) do
-    %{state | :column => col + 1, :input => rest, :results => [","|results]}
-  end
-  defp comma_impl(%ParserState{status: :ok, line: line, column: col, input: <<c::utf8,_::binary>>} = state) do
-    %{state | :status => :error, :error => "Expected , found `#{<<c::utf8>>}` at line #{line}, column #{col + 1}."}
-  end
-  defp comma_impl(%ParserState{status: :ok, input: <<>>} = state) do
-    %{state | :status => :error, :error => "Expected ,, but hit end of input."}
-  end
-
-  defparser quantifier_brace_close(%ParserState{status: :ok, column: col, input: <<?}::utf8,rest::binary>>, results: results} = state) do
-    %{state | :column => col + 1, :input => rest, :results => ["}"|results]}
-  end
-  defp quantifier_brace_close_impl(%ParserState{status: :ok, line: line, column: col, input: <<c::utf8,_::binary>>} = state) do
-    %{state | :status => :error, :error => "Expected } found `#{<<c::utf8>>}` at line #{line}, column #{col + 1}."}
-  end
-  defp quantifier_brace_close_impl(%ParserState{status: :ok, input: <<>>} = state) do
-    %{state | :status => :error, :error => "Expected }, but hit end of input."}
   end
 
   # Parse for +, * and ?
